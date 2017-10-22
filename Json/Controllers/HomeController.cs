22d767695d6b7d8e0498a6712d7128dba7775819
Józef Dabiri <yusdabiri@gmail.com>
2017-10-22 19:24:45 +0200

    feature / json serialization init

---
+++ Json/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Json.Models;
+
+namespace Json.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Json");
+        }
+
+        public JsonResult getModel()
+        {
+            ModelObj json = new ModelObj();
+            return Json(json.getModelObj(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Json/Models/ModelObj.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace Json.Models
+{
+    class ModelObj
+    {
+        public Object getModelObj(){
+           
+            string url = "http://api.icndb.com/jokes/";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
